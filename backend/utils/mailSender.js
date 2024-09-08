ac499f4d20bb923a1cc18a9f25b1b586b3fa184f
Vatsal-poddar11 <vatsalpoddar2003@gmail.com>
2024-09-08 16:13:04 +0530

    MongoDB Atlas Connection

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -10,7 +10,7 @@ const mailSender = async (email, title, body) => {
         })
 
         let info = await transporter.sendMail({
-            from: 'Medicino',
+            from: 'Medkart',
             to:`${email}`,
             subject: `${title}`,
             html: `${body}`,
