f5d938ed2d7111d4b84c5371ae804341a04c6340
Vatsal-poddar11 <vatsalpoddar2003@gmail.com>
2024-09-10 17:50:02 +0530

    Added Search bar in Catalog Page

---
+++ src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+
+const SearchBar = ({ query, setQuery }) => {
+  return (
+    <div className='flex justify-center my-4'>
+      <div className='relative w-full max-w-md'>
+        <FaSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500' />
+        <input
+          type="text"
+          placeholder="Search for medicines..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="pl-10 border border-gray-300 p-2 rounded-lg w-full"
+          style={{ maxWidth: '500px' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 import data from '../utils/medicinelist';
 import Medicines from '../components/Medicines';
+import SearchBar from '../components/SearchBar';
 
 const Catalog = () => {
+  const [query, setQuery] = useState('');
+
+  const filteredMedicines = data.filter((medicine) =>
+    medicine.name.toLowerCase().includes(query.toLowerCase()) // || medicine.info.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div className='container mx-auto p-6'>
-      <Medicines medicines={data} />
+      <SearchBar query={query} setQuery={setQuery} />
+      <Medicines medicines={filteredMedicines} />
     </div>
   );
 }
